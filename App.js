d0ec3f2c766625c15a0c4c4ad632101e21218995
AnuradhaPranav <109535563+AnuradhaPranav@users.noreply.github.com>
2023-05-20 10:14:39 +0530

    Add files via upload
    episode 1

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello world from react!");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"I am h1!"),React.createElement("h2",{},"I am h2!")]));
+root.render(parent);
